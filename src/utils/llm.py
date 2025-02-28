0a42a8b9b6a4d6379f0e437a1e6ae165fe967682
rowland <1148859424@qq.com>
2025-02-28 18:15:06 +0800

    fixed get next action body

--- src/utils/llm.py
+++ src/utils/llm.py
@@ -14,15 +14,17 @@ async def call_llm(system_content: str, human_content: str, schema: Type[BaseMod
 
 async def call_llm_with_image(system_content: str, human_content: str, schema: Type[BaseModel], image_base64: str) -> Any:
     llm = ChatOpenAI(model_name="gpt-4o")
-    system_message = SystemMessage(content=system_content)
-    human_message = HumanMessage(content=[
-        {'type': 'text', 'text': human_content},
+    messages = [
         {
-            'type': 'image_url',
-            'image_url': {'url': f'data:image/png;base64,{image_base64}'},  # , 'detail': 'low'
+            "role": "user",
+            "content": [
+                {"type": "text", "text": system_content},
+                {
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/png;base64,{image_base64}"}
+                }
+            ],
         }
-    ])
-    msg = [system_message, human_message]
-    structured_llm = llm.with_structured_output(schema=schema, include_raw=True, method="function_calling")
-    result = await structured_llm.ainvoke(msg)
-    return result   
+    ]
+    result = llm.invoke(messages)
+    return result
