41dd124d36aad4857a5cf66629440dff0e835f61
rowland <1148859424@qq.com>
2025-03-12 18:09:24 +0800

    update query prompt

--- src/strategy/server.py
+++ src/strategy/server.py
@@ -107,7 +107,7 @@ class StrategyServer:
 
 
         if strategy_doc["llm"]["is_research"]:
-            task = f'{strategy_doc["llm"]["research_content"]}'
+            task = f'{strategy_doc["llm"]["research_content"]}, \nexample:{"dataframe_id":"xxxx", "report": "xxxx"}'
             result = await fast_api.run_agent(agent_id=RESEARCH_FORMAT_AGENT_ID, task=task)
             research_result = pydash.get(result, 'data.result')
             research_result = research_result.replace("```json", "").replace("```", "")
