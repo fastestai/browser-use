ff141d8ac0a74e4e809e425c879ba3653fa22dd2
no7david <no7david@gmail.com>
2025-03-13 17:22:29 +0800

    example format

--- src/api/file_context.py
+++ src/api/file_context.py
@@ -2,9 +2,9 @@ from typing import Any
 
 
 def convert_file_context(file_meta: list[dict[str, Any]], content: str) -> str | None:
-    file_context = ""
-    for fl in file_meta:
-        file_context += f"url: {fl['source_url']}\n"
-        # todo this content is temporary, need to be replaced by the real content by fetch from the file_url or by file_id
-        file_context += f"content: {fl['content'][:5000]}\n"
-    return file_context
+	file_context = ''
+	for fl in file_meta:
+		file_context += f'url: {fl["source_url"]}\n'
+		# todo this content is temporary, need to be replaced by the real content by fetch from the file_url or by file_id
+		file_context += f'content: {fl["content"][:5000]}\n'
+	return file_context
